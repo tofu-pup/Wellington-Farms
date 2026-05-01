e8242714d3ea98c68cd784413dd3eaab5f2f1bbd
JK Markiewicz (J-K) <janmark@microsoft.com>
2026-05-02 00:08:33 +0200

    Bump SW cache to v3, switch to network-first strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wf-v1';
+const CACHE = 'wf-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -29,6 +29,12 @@ self.addEventListener('fetch', e => {
     return;
   }
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
